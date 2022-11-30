cf256004e6750aebff974fca5c2f480c29a59b3a
Marion MOTA <marion.mota@carbon-it.com>
2022-11-30 16:27:35 +0100

    [FEATURE][US_3] feat: Adding Operation formatting

--- src/main/java/com/carbon/bankaccount/account/OperationFormatter.java
+++ src/main/java/com/carbon/bankaccount/account/OperationFormatter.java
@@ -7,16 +7,39 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OperationFormatter {
+
+    public String LIBELLE_AMOUNT = "Amount";
+    public String LIBELLE_DATE = "Date";
+    public String LIBELLE_BALANCE = "Balance";
+    public String LIBELLE_OPERATION = " " + LIBELLE_DATE + "\t\t\t| " + LIBELLE_AMOUNT + "\t| "+ LIBELLE_BALANCE + "\n";
     public String LIBELLE_SOLDE = "Solde = ";
 
     DecimalFormat balanceFormat = new DecimalFormat();
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public OperationFormatter(){
         balanceFormat.setMaximumFractionDigits(2);
         balanceFormat.setMinimumFractionDigits(2);
     }
     public String formatAccountInformation(List<Operation> operations, BigDecimal balance) {
-        return formatBalance(balance);
+        String stringOperations = formatOperations(operations);
+        String stringBalance = formatBalance(balance);
+        return stringOperations + stringBalance ;
+    }
+
+    private String formatOperations(List<Operation> operations) {
+        if (operations.isEmpty()) {
+            return "";
+        }
+        return operations.stream()
+                .map(this::formatAnOperation)
+                .collect(Collectors.joining("\n", LIBELLE_OPERATION, "\n"));
+    }
+
+    private String formatAnOperation(Operation operation){
+        return operation.getDate().format(dateTimeFormatter) + "|\t\t" +
+                operation.getAmount() + "\t|\t\t" +
+                operation.getBalance();
     }
 
     private String formatBalance(BigDecimal balance) {
--- src/test/java/com.carbon.bankaccount/OperationFormatterTest.java
+++ src/test/java/com.carbon.bankaccount/OperationFormatterTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static com.carbon.bankaccount.OperationTestSample.*;
@@ -25,6 +27,17 @@ public class OperationFormatterTest {
         BigDecimal balance = BigDecimal.ZERO.setScale(2);
         String actual = formatter.formatAccountInformation(List.of(), BigDecimal.ZERO);
         assertEquals(strSolde + balance, actual);
+    }
+
+    @Test
+    @DisplayName("return all information formatted when deposit")
+    void returnFormattedAccountInformation(){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String actual = formatter.formatAccountInformation(List.of(sampleOperation), BigDecimal.ZERO);
+        String expected = new StringBuilder(" Date			| Amount	| Balance\n")
+                .append(LocalDateTime.now().format(dateTimeFormatter))
+                .append("|		0	|		0\nSolde = 0.00").toString();
+        assertEquals(expected, actual);
     }
 
 }
